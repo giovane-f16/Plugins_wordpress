8d1ae09833ae8a3988f090b84a80636aec1194c8
giovane <giovane_05silva@hotmail.com>
2022-02-10 19:51:56 -0300

    Atualizações

--- valor-dolar.php
+++ valor-dolar.php
@@ -8,7 +8,7 @@
  * 
  */
 
-add_shortcode("exemplo", function() {
+add_shortcode("valor_dolar", function() {
     // Iniciando conexão com o link que queremos acessar, enviando os parâmetros (get ?)
     $ch = curl_init("http://economia.awesomeapi.com.br/json/last/USD-BRL");
                     
@@ -26,5 +26,24 @@ add_shortcode("exemplo", function() {
 
     return $resultado["USDBRL"]["low"]; // Pegando o valor da chave low, que está dentro de dois arrays
 });
+
+add_shortcode("valor_euro", function(){
+    $ch = curl_init("http://economia.awesomeapi.com.br/json/last/EUR-BRL"); // Iniciando a conexão
+
+    curl_setopt($ch, CURLOPT_HEADER, 0); // Desativando o retorno das informações de cabeçalho
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Desativando verificação SSL
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Desativando verificação SSL
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Permitindo o retorno das informações
+
+    $res_curl = curl_exec($ch); // Executando a conexão
+
+    $resultado = json_decode($res_curl, true); // Conveterndo de Json para Array
+
+    // print_r($resultado); Outra forma de visualizar o array multidimensional
+
+    curl_close($ch); // Encerrando a conexão
+
+    return $resultado["EURBRL"]["low"]; // Dentro do Array "EURBRL", estamos pegando a informação da chave "low"
+})
 
 ?>
