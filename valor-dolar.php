f2adb057175b380a344757da3ec09a35cad91d36
giovane <giovane_05silva@hotmail.com>
2022-02-10 20:14:59 -0300

    Refatoração

--- valor-dolar.php
+++ valor-dolar.php
@@ -1,16 +1,18 @@
 <?php 
 /**
- * Plugin Name: Pegar valor do dólar
- * Description: Plugin para obter o valor do dólar 
+ * Plugin Name: Pegar valor do dólar e do euro
+ * Description: Plugin para obter o valor das cotações do Dólar e Euro
  * Version: 1.0.0
  * Author: Giovane Ferreira
  * Author URI: https://github.com/giovanef16-sys
  * 
  */
 
-add_shortcode("valor_dolar", function() {
+
+add_shortcode("valor_moeda", function($atts) {
+    $moeda = $atts["moeda"];
     // Iniciando conexão com o link que queremos acessar, enviando os parâmetros (get ?)
-    $ch = curl_init("http://economia.awesomeapi.com.br/json/last/USD-BRL");
+    $ch = curl_init("http://economia.awesomeapi.com.br/json/last/$moeda-BRL");
                     
     // Definindo configurações para a nossa conexão (Parâmetros: conexão, comando, valor)
     curl_setopt($ch, CURLOPT_HEADER, 0); // Desativa retorno das informações do cabeçalho do server
@@ -23,27 +25,7 @@ add_shortcode("valor_dolar", function() {
     $resultado = json_decode($res_curl, true); // Convertendo a string JSON para Array
         
     curl_close($ch); // Fechando a conexão
-
-    return $resultado["USDBRL"]["low"]; // Pegando o valor da chave low, que está dentro de dois arrays
+    return $resultado["{$moeda}BRL"]["low"]; // Pegando o valor da chave low, que está dentro de dois arrays
 });
-
-add_shortcode("valor_euro", function(){
-    $ch = curl_init("http://economia.awesomeapi.com.br/json/last/EUR-BRL"); // Iniciando a conexão
-
-    curl_setopt($ch, CURLOPT_HEADER, 0); // Desativando o retorno das informações de cabeçalho
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Desativando verificação SSL
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Desativando verificação SSL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Permitindo o retorno das informações
-
-    $res_curl = curl_exec($ch); // Executando a conexão
-
-    $resultado = json_decode($res_curl, true); // Conveterndo de Json para Array
-
-    // print_r($resultado); Outra forma de visualizar o array multidimensional
-
-    curl_close($ch); // Encerrando a conexão
-
-    return $resultado["EURBRL"]["low"]; // Dentro do Array "EURBRL", estamos pegando a informação da chave "low"
-})
 
 ?>
