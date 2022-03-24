3bff3772682fe00c2f32135a9def64fc6f701c3d
giovane <giovane_05silva@hotmail.com>
2022-03-24 09:54:35 -0300

    Bug de API arrumado

--- valor-moeda.php
+++ valor-moeda.php
@@ -11,7 +11,7 @@
 add_shortcode("valor_moeda", function($atts) {
     $moeda = $atts["moeda"];
     // Iniciando conexão com o link que queremos acessar, enviando os parâmetros (get ?)
-    $ch = curl_init("http://economia.awesomeapi.com.br/json/last/$moeda-BRL/");
+    $ch = curl_init("https://economia.awesomeapi.com.br/json/last/$moeda-BRL");
     // Definindo configurações para a nossa conexão (Parâmetros: conexão, comando, valor)
     curl_setopt($ch, CURLOPT_HEADER, 0); // Desativa retorno das informações do cabeçalho do server
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Desativa verificação SSL
@@ -19,11 +19,16 @@ add_shortcode("valor_moeda", function($atts) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Ativamos para permitir receber os dados enviados pelo server
     $res_curl = curl_exec($ch); // Executamos a nossa conexão
     $resultado = json_decode($res_curl, true); // Convertendo a string JSON para Array
-    var_dump($resultado);
+    //var_dump($resultado);
     curl_close($ch); // Fechando a conexão
-    return $resultado["{$moeda}BRL"]["low"]; // Pegando o valor da chave low, que está dentro de dois arrays
+    return $resultado["{$moeda}BRL"]["high"]; // Pegando o valor da chave high, que está dentro do array [USDBRL]
 });
 
+// acessar URL: https://economia.awesomeapi.com.br/json/last/USD-BRL 
+// var dump na variável resultado: NULL
+// apagar chave ["low"]: sai uma linha de erro
+// criar função de teste: funcionou
+// documentação: nada de específico
+// trocar URL: http -> https: deu certo 
+
 ?>
-
-
