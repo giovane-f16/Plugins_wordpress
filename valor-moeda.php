e55ec43ac2ce7b12918159c92b26383158316d83
giovane <giovane_05silva@hotmail.com>
2022-04-27 11:30:36 -0300

    Função criptomoedas

--- valor-moeda.php
+++ valor-moeda.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * Plugin Name: Cotação moedas - USD/EUR
+ * Plugin Name: Cotação moedas
  * Description: Plugin para obter o valor das cotações do Dólar e Euro
  * Version: 1.0.0
  * Author: Giovane Ferreira
@@ -22,6 +22,23 @@ add_shortcode("valor_moeda", function($atts) {
     // Pegando o valor da chave high, que está dentro do array [USDBRL]
     $num = $resultado["{$moeda}BRL"]["low"];
     return substr($num, 0, 4); // retornando a string tratada
+});
+
+add_shortcode("criptomoedas", function($atts){
+    $entrada = $atts["entrada"];
+
+    $ch = curl_init("https://api.coinranking.com/v2/coins");
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    $res_curl = curl_exec($ch); 
+    $resultado = json_decode($res_curl, true);
+    curl_close($ch);
+    
+    $formatado = (float)$resultado["data"]["coins"][$entrada]["price"];
+
+    return number_format($formatado, 2, ',', '.');
 });
 
 ?>
